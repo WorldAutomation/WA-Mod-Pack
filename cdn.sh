e9f4e3ef4bc5165738162be1099b658e5207c6c2
mindamp <mindamp@gmail.com>
2018-11-30 15:26:26 -0600

    Bleh... automation and stuff.

--- cdn.sh
+++ cdn.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-cd /var/resist/resist-mod-pack;
-/var/resist/resist-mod-pack/pull.sh;
+cd ~/WA-Mod-Pack;
+./pull.sh;
 npm update http-server -g;
-http-server -p 9090 /var/resist/resist-mod-pack;
+http-server -p 9090 .;
 
