92deffef310174b231699456ad5ad8e261d60a8a
mindamp <mindamp@gmail.com>
2018-11-30 19:15:46 -0600

    Bleh... automation and stuff.

--- distribution.js
+++ distribution.js
@@ -3,7 +3,7 @@ fs = require('fs'),
 execSync = require('child_process').execSync,
 dir = require('node-dir'),
 md5File = require('md5-file'),
-downloadCDN = 'https://cdn.worldautomation.net/'
+downloadCDN = 'http://cdn.worldautomation.net/'
 function getFilesizeInBytes(filename) {
 	const stats = fs.statSync(filename)
 	const fileSizeInBytes = stats.size
