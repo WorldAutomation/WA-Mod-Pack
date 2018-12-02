93ee6c403a76ea1fc304584fa7be76c82529c129
mindamp <mindamp@gmail.com>
2018-12-02 10:03:51 -0600

    Bleh... automation and stuff.

--- start_distro.sh
+++ start_distro.sh
@@ -5,3 +5,5 @@ node distribution.js
 jsonlint -i distribution.json
 cp -rf distribution.json ../WA-Launcher/app/assets/.
 cp -rf distribution.json dist/distribution.json
+latest=$(wget -qO- https://minecraft.curseforge.com/projects/worldautomation | cat | grep "projects/worldautomation/files/" | awk -F "\"" '{ print $4 }')
+curl "https://minecraft.curseforge.com$latest" > curse.html
