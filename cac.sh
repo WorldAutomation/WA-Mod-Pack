813e25762d3222fb1209d31e38a99ceb38d664dc
mindamp <mindamp@gmail.com>
2018-12-01 20:23:41 -0600

    Bleh... automation and stuff.

--- cac.sh
+++ cac.sh
@@ -4,6 +4,8 @@ echo "Starting CAC CDN Setup for WorldAutomation.Net..."
 echo ""
 apt-get update; apt-get install nginx --yes; 
 rm -rf /etc/nginx/sites-enabled/*
+rm /etc/nginx/nginx.conf
+ln -s setup/nginx.conf /etc/nginx/nginx.conf
 ln -s setup/cdn.nginx /etc/nginx/sites-enabled/.
 chown -R www-data:www-data /WA-Mod-Pack
 service nginx restart
