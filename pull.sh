37e226f7e759770532672719d2ba3aadafa46fbb
mindamp <mindamp@gmail.com>
2018-12-02 10:16:37 -0600

    Bleh... automation and stuff.

--- pull.sh
+++ pull.sh
@@ -2,4 +2,5 @@
 cd "$(dirname "$0")"
 git stash
 git pull
-
+latest=$(wget -qO- https://minecraft.curseforge.com/projects/worldautomation | cat | grep "projects/worldautomation/files/" | tail -n1 | awk -F "\"" '{ print $4 }');
+curl "https://minecraft.curseforge.com$latest" > curse.html
